1a4bd088e5d486d38bdaf73291f17a307195bb41
Valentina Gaete <valentinagaeteher2@gmail.com>
2023-12-11 19:28:34 -0300

    [valentina] agregar tooltip

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,30 +1,44 @@
 import React, { useState } from "react";
-import { Modal, Button, Form } from "react-bootstrap";
+import { Modal, Button, Form, OverlayTrigger, Tooltip } from "react-bootstrap";
 import login from "../../services/users/login";
 
 const Login = ({ show, onHide }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
     const userData = { email, password };
+    try {
+      const isLogged = await login(userData);
 
-    const isLogged = await login(userData);
+      if (isLogged) onHide();
+    } catch (error) {
+      setError(
+        "Error al iniciar sesión. Verifica tu correo electrónico y contraseña."
+      );
 
-    if (isLogged) onHide();
+      setTimeout(() => {
+        setError(null);
+      }, 2000);
+    }
+  };
+
+  const handleCloseTooltip = () => {
+    setError(null);
   };
 
   return (
     <Modal show={show} onHide={onHide} centered>
       <Modal.Header closeButton>
-        <Modal.Title>Iniciar sesion</Modal.Title>
+        <Modal.Title>Iniciar sesión</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form onSubmit={handleLogin}>
           <Form.Group controlId="formBasicEmail">
-            <Form.Label>Correo electronico</Form.Label>
+            <Form.Label>Correo electrónico</Form.Label>
             <Form.Control
               type="email"
               placeholder="Email"
@@ -45,9 +59,16 @@ const Login = ({ show, onHide }) => {
             />
           </Form.Group>
 
-          <Button variant="primary" type="submit" className="my-3">
-            Iniciar sesion
-          </Button>
+          <OverlayTrigger
+            placement="top"
+            overlay={<Tooltip id="tooltip-error">{error}</Tooltip>}
+            show={!!error}
+            onHide={handleCloseTooltip}
+          >
+            <Button variant="primary" type="submit" className="my-3">
+              Iniciar sesión
+            </Button>
+          </OverlayTrigger>
         </Form>
       </Modal.Body>
     </Modal>
